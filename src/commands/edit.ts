a01942bbcd6802cf8fcdb6cbef23ae148ee789f9
Danny Tuppeny <danny@tuppeny.com>
2018-03-07 08:31:41 +0000

    Select text as you click tree nodes
    See #605.

--- src/commands/edit.ts
+++ src/commands/edit.ts
@@ -17,11 +17,19 @@ export class EditCommands implements vs.Disposable {
 			vs.commands.registerCommand("_dart.organizeImports", this.organizeImports, this),
 			vs.commands.registerCommand("dart.sortMembers", this.sortMembers, this),
 			vs.commands.registerCommand("_dart.applySourceChange", this.applyEdits, this),
+			vs.commands.registerCommand("_dart.highlightRange", this.highlightRange, this),
 		);
 	}
 
 	private organizeImports(): Thenable<void> {
 		return this.sendEdit(this.analyzer.editOrganizeDirectives, "Organize Imports");
+	}
+
+	private highlightRange(editor: vs.TextEditor, range: vs.Range) {
+		// TODO: We don't have a way of highlighting so for now we just move cursor there
+		// See https://github.com/Microsoft/vscode/issues/45059
+		editor.selection = new vs.Selection(range.start, range.end);
+		vs.window.showTextDocument(editor.document);
 	}
 
 	private sortMembers(): Thenable<void> {
